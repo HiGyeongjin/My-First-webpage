5d2672032eb857e00719f7d2f6e8f2b25e11826c
Gyeongjin Lee <asdfg031897@gmail.com>
2023-11-10 19:19:06 -0800

    move content of header to main

--- Portfolio/src/App.js
+++ Portfolio/src/App.js
@@ -1,11 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 import Main from "./component/main";
+import Header from "./component/Header";
 import "./App.css";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Main />} />
+      {/* <Route path="/about" element={<Header />} /> */}
     </Routes>
   );
 }
--- Portfolio/src/component/main.jsx
+++ Portfolio/src/component/main.jsx
@@ -1,10 +1,15 @@
-import Header from "./header";
+import Header from "./Header";
 import React from "react";
 
 export default function Main() {
   return (
     <div>
-      <Header />
+      <ul>
+        <li>Home</li>
+        <li>About me</li>
+        <li>Skill</li>
+        <li>Contact</li>
+      </ul>
       <h1>Main page</h1>
     </div>
   );
